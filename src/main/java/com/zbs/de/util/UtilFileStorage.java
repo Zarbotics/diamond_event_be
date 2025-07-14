73048bd6c2cb52681e8a398de5baf9123a6087a3
Qasim Hameed <qhameed456@gmail.com>
2025-07-14 15:22:13 +0500

    Image Folder Path For Server.

--- src/main/java/com/zbs/de/util/UtilFileStorage.java
+++ src/main/java/com/zbs/de/util/UtilFileStorage.java
@@ -11,15 +11,15 @@ import org.springframework.web.multipart.MultipartFile;
 public class UtilFileStorage {
 
 	public static String saveFile(MultipartFile file, String category) throws IOException {
-		//String folder = "/data/uploads/" + category;
-		String folder = "D:/Zarbotics/UploadedFiles/" +category;
+		// String folder = "/data/uploads/" + category;
+		String folder = "/root/diamondevent_be/uploads/" + category;
 		Files.createDirectories(Paths.get(folder));
 
 		String fileName = UUID.randomUUID() + "_" + file.getOriginalFilename();
 		Path filePath = Paths.get(folder, fileName);
 		file.transferTo(filePath);
 
-		return filePath.toString(); 
+		return filePath.toString();
 	}
 
 }
