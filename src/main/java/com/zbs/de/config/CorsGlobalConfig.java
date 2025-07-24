4bbeffb2b3a3e424dd578a9b27c8b540f70d2fe8
Qasim Hameed <qhameed456@gmail.com>
2025-07-24 19:12:03 +0500

    Origin allowed to All (Talha Requested)

--- src/main/java/com/zbs/de/config/CorsGlobalConfig.java
+++ src/main/java/com/zbs/de/config/CorsGlobalConfig.java
@@ -8,15 +8,26 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsGlobalConfig {
 
+//	@Bean
+//	public WebMvcConfigurer corsConfigurer() {
+//		return new WebMvcConfigurer() {
+//			@Override
+//			public void addCorsMappings(CorsRegistry registry) {
+//				registry.addMapping("/**")
+//						.allowedOrigins("http://localhost:5173", "https://localhost:5173", "http://87.106.101.41","https://frosty-jang.87-106-101-41.plesk.page:8081",
+//								"https://87.106.101.41", "https://frosty-jang.87-106-101-41.plesk.page")
+//						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS").allowedHeaders("*")
+//						.allowCredentials(true);
+//			}
+//		};
+//	}
+
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-						.allowedOrigins("http://localhost:5173", "https://localhost:5173", "http://87.106.101.41","https://frosty-jang.87-106-101-41.plesk.page:8081",
-								"https://87.106.101.41", "https://frosty-jang.87-106-101-41.plesk.page")
-						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS").allowedHeaders("*")
+				registry.addMapping("/").allowedOrigins("*").allowedMethods("*").allowedHeaders("*")
 						.allowCredentials(true);
 			}
 		};
