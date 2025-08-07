a9dade00f9568f6ce2b73b70f5b26f9cf4f1c690
Qasim Hameed <qhameed456@gmail.com>
2025-08-07 14:31:53 +0500

    bug Fixed

--- src/main/java/com/zbs/de/service/impl/ServiceDecorCategoryPropertyValueDocumentImpl.java
+++ src/main/java/com/zbs/de/service/impl/ServiceDecorCategoryPropertyValueDocumentImpl.java
@@ -1,20 +1,22 @@
 package com.zbs.de.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.zbs.de.model.DecorCategoryPropertyValueDocument;
 import com.zbs.de.repository.RepositoryDecorCategoryPropertyValueDocument;
 import com.zbs.de.service.ServiceDecorCategoryPropertyValueDocument;
 
+@Service("serviceDecorCategoryPropertyValueDocumentImpl")
 public class ServiceDecorCategoryPropertyValueDocumentImpl implements ServiceDecorCategoryPropertyValueDocument {
-	
+
 	@Autowired
 	RepositoryDecorCategoryPropertyValueDocument repositoryDecorCategoryPropertyValueDocument;
 
 	@Override
 	public DecorCategoryPropertyValueDocument save(DecorCategoryPropertyValueDocument document) {
-		if(document != null) {
-			return  repositoryDecorCategoryPropertyValueDocument.save(document);
+		if (document != null) {
+			return repositoryDecorCategoryPropertyValueDocument.save(document);
 		}
 		return null;
 	}
