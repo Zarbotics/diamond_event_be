3bb754c9bd02da8265da0e9f55a53b8cd3c84327
Qasim Hameed <qhameed456@gmail.com>
2025-09-01 18:06:23 +0500

    Decor Extras Fixed

--- src/main/java/com/zbs/de/model/EventMaster.java
+++ src/main/java/com/zbs/de/model/EventMaster.java
@@ -152,7 +152,7 @@ public class EventMaster extends BaseEntity implements Serializable {
 	@OneToMany(mappedBy = "eventMaster", cascade = CascadeType.ALL, orphanRemoval = true)
 	private List<EventDecorCategorySelection> decorSelections = new ArrayList<>();
 
-	@OneToMany(mappedBy = "eventMaster")
+	@OneToMany(mappedBy = "eventMaster", cascade = CascadeType.ALL, orphanRemoval = true)
 	private List<EventDecorExtrasSelection> extrasSelections;
 
 	public Integer getSerEventMasterId() {
