8ddad3bc9146c33c9530a95df7b07749097f959c
Qasim Hameed <qhameed456@gmail.com>
2025-09-01 17:41:37 +0500

    image path

--- src/main/java/com/zbs/de/controller/ControllerFileServe.java
+++ src/main/java/com/zbs/de/controller/ControllerFileServe.java
@@ -32,35 +32,13 @@ public class ControllerFileServe {
 //		return ResponseEntity.ok().contentType(MediaType.APPLICATION_OCTET_STREAM).body(resource);
 //	}
 
-	
-//  //This is Updated Code For Production Environment to view the images in browser instead of download them in the system and then view 
-//	@GetMapping("/{category}/{filename:.+}")
-//	public ResponseEntity<Resource> serveFile(@PathVariable String category, @PathVariable String filename)
-//			throws IOException {
-//
-//		Path filePath = Paths.get("/root/diamondevent_be/uploads/" + category + "/" + filename);
-//		if (!Files.exists(filePath)) {
-//			return ResponseEntity.notFound().build();
-//		}
-//
-//		// Detect MIME type
-//		String contentType = Files.probeContentType(filePath);
-//		if (contentType == null) {
-//			contentType = "application/octet-stream";
-//		}
-//
-//		UrlResource resource = new UrlResource(filePath.toUri());
-//		return ResponseEntity.ok().contentType(MediaType.parseMediaType(contentType)).body(resource);
-//	}
-
-	
-	
-	// This is for local Machine
+	// This is Updated Code For Production Environment to view the images in browser
+	// instead of download them in the system and then view
 	@GetMapping("/{category}/{filename:.+}")
 	public ResponseEntity<Resource> serveFile(@PathVariable String category, @PathVariable String filename)
 			throws IOException {
 
-		Path filePath = Paths.get("D:/Zarbotics/UploadedFiles/", category, filename);
+		Path filePath = Paths.get("/root/diamondevent_be/uploads/" + category + "/" + filename);
 		if (!Files.exists(filePath)) {
 			return ResponseEntity.notFound().build();
 		}
@@ -74,4 +52,24 @@ public class ControllerFileServe {
 		UrlResource resource = new UrlResource(filePath.toUri());
 		return ResponseEntity.ok().contentType(MediaType.parseMediaType(contentType)).body(resource);
 	}
+
+//	// This is for local Machine
+//	@GetMapping("/{category}/{filename:.+}")
+//	public ResponseEntity<Resource> serveFile(@PathVariable String category, @PathVariable String filename)
+//			throws IOException {
+//
+//		Path filePath = Paths.get("D:/Zarbotics/UploadedFiles/", category, filename);
+//		if (!Files.exists(filePath)) {
+//			return ResponseEntity.notFound().build();
+//		}
+//
+//		// Detect MIME type
+//		String contentType = Files.probeContentType(filePath);
+//		if (contentType == null) {
+//			contentType = "application/octet-stream";
+//		}
+//
+//		UrlResource resource = new UrlResource(filePath.toUri());
+//		return ResponseEntity.ok().contentType(MediaType.parseMediaType(contentType)).body(resource);
+//	}
 }
